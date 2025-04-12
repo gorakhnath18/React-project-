c30a97770be4b76b212d1d654c98eed57d0fc4ff
git config --global user.name <gorakhnathx34@gmail.com>
2025-04-12 13:21:23 +0530

    a

--- themechanger/src/App.jsx
+++ themechanger/src/App.jsx
@@ -15,8 +15,7 @@ function App() {
     setThemeMode("dark")
   }
 
-  // actual change in theme
-
+ 
   useEffect(() => {
     document.querySelector('html').classList.remove("light", "dark")
     document.querySelector('html').classList.add(themeMode)
